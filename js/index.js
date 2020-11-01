623ca40750707e409bb8d881c663884b74e1dc1a
Abortos <javieralvarez2.ja@gmail.com>
2020-11-01 17:40:26 +0100

    trying modals

--- js/index.js
+++ js/index.js
@@ -5,8 +5,9 @@ import { setupCards } from "./cards.js";
 document.addEventListener("DOMContentLoaded", () => {
   "use strict";
 
-  let mierdaButton = document.querySelector("#mierda-button");
   let upButton = document.querySelector("#up-button");
+  let modal = document.querySelector(".modal");
+  let instances = M.modal.init(modal, options);
 
   window.onscroll = () => {
     document.body.scrollTop > 40 || document.documentElement.scrollTop > 40
@@ -14,14 +15,11 @@ document.addEventListener("DOMContentLoaded", () => {
       : (upButton.style.opacity = "0");
   };
 
-  mierdaButton.onclick = () => {
-    alert("Eres un mierdas... ¡Y lo sabes!");
-  };
-
   upButton.onclick = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   };
+
 
   setupSidebar(mierdas);
   setupCards(mierdas);
